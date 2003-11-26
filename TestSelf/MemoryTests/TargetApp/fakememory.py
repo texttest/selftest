fbe7df0fd7637442eb9017efa193e3d13a2cf4bd
geoff <>
2003-11-26 15:36:22 +0000

    Added a load of self-tests for memory checking

---
+++ TestSelf/MemoryTests/TargetApp/fakememory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def writeMemory(mem):
+    print "Memory usage now", mem, "MB" 
+
+# Just print a bunch of numbers and an intro. Pretend to be writing memory numbers
+print "Running Fake Memory Program..."
+writeMemory(35.6)
+writeMemory(47.2)
+writeMemory(47.8)
+writeMemory(42.3)
+writeMemory(38.4)
