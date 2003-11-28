8abe9e3e4ef5040b7b51a7766c7c79647b624d9d
geoff <>
2003-11-28 14:05:54 +0000

    Changed the tests to check for tables

--- TestSelf/RunDependentText/TargetApp/writeessay.py
+++ TestSelf/RunDependentText/TargetApp/writeessay.py
@@ -5,10 +5,10 @@ print os.linesep + "Sometimes we remove line number 2..." + os.linesep
 
 print "Sometimes we remove today's date, 2003-12-01T23:24" + os.linesep
 
-print "Or even several lines at once:"
-print "This"
-print "That"
-print "And this one" + os.linesep
+print "Or even several lines at once, like in this table:"
+print "   Here     are     some     columns"
+print "   That    have    spacing   between"
+print "Repeating     3     3     3     3" + os.linesep
 
 print "Sometimes between markers, starting section"
 print "Rant"
