537fd3df8fd16ead5ce6f832ebdd566b9a851b90
geoff <>
2003-11-28 09:28:53 +0000

    Self-tests for the various forms of run-dependent text

---
+++ TestSelf/RunDependentText/TargetApp/writeessay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+
+print os.linesep + "Sometimes we remove line number 2..." + os.linesep
+
+print "Sometimes we remove today's date, 2003-12-01T23:24" + os.linesep
+
+print "Or even several lines at once:"
+print "This"
+print "That"
+print "And this one" + os.linesep
+
+print "Sometimes between markers, starting section"
+print "Rant"
+print "Rave"
+print "Gibber"
+print "End Section" + os.linesep
+
+print "This should always go"
