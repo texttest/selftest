03b2070947f2faf1e2a99ba75fa197c8600de14a
geoff <>
2004-02-24 13:40:04 +0000

    Changed NightjobTerminate test to run for longer and be less prone to race conditions

--- lsf/TargetApp/sleep.py
+++ lsf/TargetApp/sleep.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-import time
+import time, sys
 
+sleepLength = 5
+if len(sys.argv) > 1:
+    sleepLength = int(sys.argv[1])
 print "Sleeping for 5 seconds..."
 time.sleep(5)
 print "Done"
