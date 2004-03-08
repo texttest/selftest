b2aae12c8a65326c29ddacec7cad3a131cf56439
geoff <>
2004-03-08 09:53:00 +0000

    Broken somewhat since ported to Bourne Shell

--- lsf/TargetApp/sleep.sh
+++ lsf/TargetApp/sleep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sleepLength=5
-if [ -s "$1" ]; then
+if [ -n "$1" ]; then
     sleepLength=$1
 fi
-echo "Sleeping for 5 seconds..."
-sleep 5
+echo "Sleeping for $sleepLength seconds..."
+sleep $sleepLength
 echo "Done"
