6d11df146964997708234d044056a4aabf050574
geoff <>
2005-01-26 13:31:42 +0000

    New test for fix of bug 3037

--- TestSelf/RunDependentText/TargetApp/writeessay.py
+++ TestSelf/RunDependentText/TargetApp/writeessay.py
@@ -5,6 +5,9 @@ print os.linesep + "Sometimes we remove line number 2..." + os.linesep
 
 print "Sometimes we remove today's date, 2003-12-01T23:24" + os.linesep
 
+print "We need to be able to match on trailing spaces "
+print " and also on leading spaces" + os.linesep
+
 print "Or even several lines at once, like in this table:"
 print "   Here     are     some     columns"
 print "   That    have    spacing   between"
