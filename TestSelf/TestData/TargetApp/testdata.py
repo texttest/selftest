8939222bd413ca5c4d1cc35f904f87796559a52c
geoff <>
2006-04-20 11:31:39 +0000

    Added test to make sure we can link test paths from subdirectories correctly

--- TestSelf/TestData/TargetApp/testdata.py
+++ TestSelf/TestData/TargetApp/testdata.py
@@ -20,13 +20,16 @@ def editFiles(dir):
 
 file = open("readonlyfile")
 print "Found and read my read-only file: ", file.read()
+if os.path.isfile("readonlyfile2"):
+    file = open("readonlyfile2")
+    print "Found and read my second read-only file: ", file.read()
 
 envVar = os.getenv("MY_ENV_VAR")
 if envVar:
     print "Got environment variable", envVar
 
 writedirs = os.getenv("WRITEABLE_DIRS") 
-if os.path.isdir(writedirs):
+if writedirs and os.path.isdir(writedirs):
     editFiles(writedirs)
 else:
     print "Didn't get given a directory structure to edit!"
