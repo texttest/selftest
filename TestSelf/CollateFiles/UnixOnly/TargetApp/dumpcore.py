82e5e01c95f2795b7ba159b44e01d6e582deafc5
geoff <>
2006-05-08 11:42:02 +0000

    Generalised core-interpreting tests so they will work anywhere with any Python version. Introduced config file entrry for collate_script

--- TestSelf/CollateFiles/UnixOnly/TargetApp/dumpcore.py
+++ TestSelf/CollateFiles/UnixOnly/TargetApp/dumpcore.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 
-import os, time, sys
+import os
 
-if len(sys.argv) == 1:
-    processId = os.fork()
-    if processId == 0:
-        time.sleep(10)
-    else:
-        time.sleep(1)
-        print "Killing child process with signal 11"
-        os.system("kill -11 " + str(processId))
-        os.waitpid(processId, 0)
-else:
-    # Produce fake core
-    os.system("which grep > core")
+# Produce fake core
+file = open("core", "w")
+file.write(os.getenv("COREFILE_BINARY"))
