58e3c4891eac0666616a1086a0a629ad0fe6dbca
geoff <>
2006-08-15 10:26:23 +0000

    Reworked tests a bit to try to cover a few more cases

--- TestSelf/TrafficInterception/TargetApp/source/secondcall.py
+++ TestSelf/TrafficInterception/TargetApp/source/secondcall.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 import os, sys
-print "secondcall.py called with", sys.argv[1:], "as arguments"
+sys.stderr.write("secondcall.py called with " + repr(sys.argv[1:]) + " as arguments\n")
 print os.getpid()
--- TestSelf/TrafficInterception/TargetApp/source/syscalls.py
+++ TestSelf/TrafficInterception/TargetApp/source/syscalls.py
@@ -2,5 +2,5 @@
 
 import os
 print "In syscalls.py now"
-os.system("firstcall.py -a -b -c hello")
+os.system("firstcall.py -a -b -c 'hello | goodbye'")
 os.system("secondcall.py -c -x")
