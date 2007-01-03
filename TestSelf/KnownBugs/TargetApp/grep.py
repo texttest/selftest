c32a76f325b017458e17c1e7c46e66073012b0db
geoff <>
2007-01-03 17:16:43 +0000

    Have to check this in to be able to test...

--- TestSelf/KnownBugs/TargetApp/grep.py
+++
@@ -1,6 +0,0 @@
-# This is a very inferior version of grep, but sufficient for our purposes...
-import sys
-pattern = sys.argv[1]
-for line in sys.stdin.readlines():
-    if line.find(pattern) != -1:
-        print line
