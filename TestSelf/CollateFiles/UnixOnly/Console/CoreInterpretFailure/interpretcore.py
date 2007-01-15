4a6612a5f9a552cc5317b3e4d5b7c7cb4dc0715d
geoff <>
2007-01-15 15:56:17 +0000

    Don't test interpretcore.py here, we do that elsewhere as we don't trust it anyway...

--- TestSelf/CollateFiles/UnixOnly/Console/CoreInterpretFailure/interpretcore.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-sys.stderr.write("interpretcore.py had a bad day and probably threw a nasty exception\n")
--- TestSelf/CollateFiles/UnixOnly/TargetApp/dumpcore.py
+++ TestSelf/CollateFiles/UnixOnly/TargetApp/dumpcore.py
@@ -4,7 +4,5 @@ import os
 
 # Produce fake core
 file = open("core", "w")
-binary = os.getenv("COREFILE_BINARY")
-if binary:
-    file.write(binary + "\0")
+file.write("Dummy core\n")
 file.close()
