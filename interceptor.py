92719a1bc766e7c9ef43a55c6871b626430b9680
geoff <>
2007-01-22 15:33:51 +0000

    Fixing some batch tests' output

--- interceptor.py
+++ interceptor.py
@@ -3,7 +3,7 @@ import os, smtplib
 
 class SMTP(smtplib.SMTP):
     def sendmail(self, fromAddr, toAddresses, contents):
-        print "Sending mail from address", fromAddr, "to", toAddresses
+        print "\nSending mail from address", fromAddr, "to", toAddresses
         fileName =  os.path.join(os.path.dirname(os.getenv("TEXTTEST_TMP")), "targetReport.texttest")
         writeFile = open(fileName, "w")
         writeFile.write(contents)
