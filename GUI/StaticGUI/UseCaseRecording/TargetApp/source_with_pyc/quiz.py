269358c56a828542476ced4cb86445aa9c826a38
geoff <>
2007-02-26 15:26:36 +0000

    Removing all code to do with recording standard input

--- GUI/StaticGUI/UseCaseRecording/TargetApp/source_with_pyc/quiz.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from usecase import ScriptEngine
-
-scriptEngine = ScriptEngine()
-print "What is your name?"
-answer = scriptEngine.readStdin()
-print "Your name contains", len(answer.strip()), "characters"
