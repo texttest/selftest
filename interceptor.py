04f545d0515502fa4d359f6938be4cf9d8f062a4
geoff <>
2007-05-07 11:31:50 +0000

    Must mock out the mailserver also, there might not be one

--- interceptor.py
+++ interceptor.py
@@ -2,10 +2,14 @@
 import os, smtplib
 
 class SMTP(smtplib.SMTP):
+    def connect(self, server):
+        pass # Fake this so we don't rely on a mail server to run the tests
     def sendmail(self, fromAddr, toAddresses, contents):
         print "\nSending mail from address", fromAddr, "to", toAddresses
         fileName =  os.path.join(os.path.dirname(os.getenv("TEXTTEST_TMP")), "targetReport.texttest")
         writeFile = open(fileName, "w")
         writeFile.write(contents)
+    def quit(self):
+        pass
 
 smtplib.SMTP = SMTP
