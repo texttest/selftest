d4b080018df803471841e7b202645800a3f81a48
geoff <>
2007-05-07 11:49:55 +0000

    Need to fake out the server, which might not exist for real

--- TestSelf/BatchTests/Console/NoSuchSender/interceptor.py
+++ TestSelf/BatchTests/Console/NoSuchSender/interceptor.py
@@ -5,6 +5,10 @@ class SMTPSenderRefused(Exception):
     pass
 
 class SMTP(smtplib.SMTP):
+    def connect(self, server):
+        pass
+    def quit(self, server):
+        pass
     def sendmail(self, fromAddr, to, contents):
         raise SMTPSenderRefused, "5.5.4 <" + fromAddr + ">... Domain name required for sender address " + fromAddr
 
