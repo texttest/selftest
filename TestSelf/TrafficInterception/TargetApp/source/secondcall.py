3b840b59f8cc06fbcfc434e038fc154fde9cd09f
geoff <>
2007-05-28 08:08:27 +0000

    Change working directory to see if it confuses the code...

--- TestSelf/TrafficInterception/TargetApp/source/secondcall.py
+++ TestSelf/TrafficInterception/TargetApp/source/secondcall.py
@@ -3,4 +3,9 @@
 import os, sys
 sys.stderr.write("secondcall.py called with " + repr(sys.argv[1:]) + " as arguments\n" + \
                  "MY_ENV_VAR = " + repr(os.getenv("MY_ENV_VAR")) + "\n")
+
+# Are we in the right working directory?
+if os.getcwd() != os.getenv("TEXTTEST_TMP"):
+    sys.stderr.write("We're in the wrong place - " + os.getcwd() + "\n" + os.getenv("TEXTTEST_TMP") + "\n")
+
 print "The process ID is", os.getpid()
--- TestSelf/TrafficInterception/TargetApp/source/syscalls.py
+++ TestSelf/TrafficInterception/TargetApp/source/syscalls.py
@@ -13,6 +13,7 @@ sys.stdout.flush()
 # Repeat the first command again, it might cause problems (!)
 os.system('firstcall.py -a -b -c "hello | goodbye"')
 
-# Check our environment goes into the traffic environment
+# Check our environment and current working directory goes into the traffic environment
 os.environ["MY_ENV_VAR"] = "value"
+os.chdir(os.getenv("TEXTTEST_TMP"))
 os.system("secondcall.py -c -x")
