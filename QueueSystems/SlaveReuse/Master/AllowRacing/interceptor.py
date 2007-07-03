978791e2df240e73bcb4f33cf68503e6f2f89515
geoff <>
2007-07-03 07:16:36 +0000

    New tests for slave reuse, and cut some tests out of LSF tests that aren't grid-engine specific

---
+++ QueueSystems/SlaveReuse/Master/AllowRacing/interceptor.py
@@ -0,0 +1,15 @@
+
+import subprocess
+from time import sleep
+
+RealPopen = subprocess.Popen
+class SlowPopen(RealPopen):
+    doneSleep = False
+    def __init__(self, cmdArgs, *args, **kwargs):
+        RealPopen.__init__(self, cmdArgs, *args, **kwargs)
+        if not self.doneSleep and (cmdArgs[0] == "qsub" or cmdArgs[0] == "bsub"):
+            SlowPopen.doneSleep = True
+            print "Waiting Q thread for 5 seconds"
+            sleep(5)
+        
+subprocess.Popen = SlowPopen
