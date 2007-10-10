7f61c4666eefa704b928e619b64e1b59177c879c
geoff <>
2007-10-10 14:18:42 +0000

    Better to remove the KeyboardInterrupt exception at source, then we also get correct return codes

--- TestSelf/RunningTests/TargetApp/sleep.py
+++ TestSelf/RunningTests/TargetApp/sleep.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import sys, time, os
+import sys, time, os, signal
+
+
+# Don't print stack traces when killed, as Windows can't do this...
+if os.name == "posix":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def getSleepLength():
     return int(os.getenv("SLEEP_LENGTH", 1000))
@@ -8,8 +13,5 @@ def getSleepLength():
 sleepLength = getSleepLength()
 print "Sleeping for", sleepLength, "seconds..."
 sys.stdout.flush()
-try:
-    time.sleep(sleepLength)
-    print "Done"
-except:
-    pass # Don't print stack traces when killed, as Windows can't do this...
+time.sleep(sleepLength)
+print "Done"
