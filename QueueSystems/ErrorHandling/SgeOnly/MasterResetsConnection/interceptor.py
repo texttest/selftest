af9af3101c8385f88287d174a07a0b7fe9123922
geoff <>
2007-10-19 11:52:11 +0000

    See what happens if we forcibly reset the connection

---
+++ QueueSystems/ErrorHandling/SgeOnly/MasterResetsConnection/interceptor.py
@@ -0,0 +1,15 @@
+
+import socket
+
+origSocket = socket.socket
+
+class ResettingSocket(origSocket):
+    callCount = 0
+    def makefile(self, *args, **kwargs):
+        ResettingSocket.callCount += 1
+        if ResettingSocket.callCount == 2:
+            raise socket.error, "Connection reset by peer"
+        else:
+            return origSocket.makefile(self, *args, **kwargs)
+
+socket.socket = ResettingSocket
