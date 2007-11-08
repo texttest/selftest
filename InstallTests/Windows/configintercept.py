9bec79264763af2f4d1c001aeb5770ae9058925f
geoff <>
2007-11-08 10:23:54 +0000

    New tests for programs that aren't installed

--- InstallTests/Windows/configintercept.py
+++ InstallTests/Windows/configintercept.py
@@ -1,2 +1,13 @@
 import os
 os.name = "nt"
+
+orig_isfile = os.path.isfile
+
+def my_isfile(path):
+    programs = [ "tkdiff.exe", "java.exe", "baretail.exe" ]
+    if os.path.basename(path) in programs:
+        return True
+    else:
+        return orig_isfile(path)
+
+os.path.isfile = my_isfile
