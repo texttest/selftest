3d50f8efb0d1fd533eb8fba70b2fdfc0c06109a3
mattias <>
2007-11-08 22:42:33 +0000

    Bugzilla 21102 - Always use default encoding ISO8859-1 to make sure tests with Swedish chars work ok also on systems which have a different default encoding.

--- interceptor.py
+++ interceptor.py
@@ -13,3 +13,13 @@ class SMTP(smtplib.SMTP):
         pass
 
 smtplib.SMTP = SMTP
+
+# Make sure the tests with Swedish chars work correctly
+# on systems with other default locales than ISO8859-1 ...
+
+import locale
+
+def default_locale():
+    return "en_US", "ISO8859-1"
+
+locale.getdefaultlocale = default_locale
