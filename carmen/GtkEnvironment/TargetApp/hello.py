ba361ea6004260be54ae8bc2a7d28104c4aa616a
geoff <>
2008-01-15 16:00:11 +0000

    Make the test more resilient

--- carmen/GtkEnvironment/TargetApp/hello.py
+++ carmen/GtkEnvironment/TargetApp/hello.py
@@ -4,5 +4,5 @@ import gtk, sys, os
 
 print "Python version =", ".".join(map(str, sys.version_info[:3]))
 print "GTK version =", ".".join(map(str, gtk.gtk_version))
-print "LD_LIBRARY_PATH =", os.getenv("LD_LIBRARY_PATH").split(":")
-print "PYTHONPATH =", os.getenv("PYTHONPATH").split(":")
+print "LD_LIBRARY_PATH starts with", os.getenv("LD_LIBRARY_PATH").split(":")[0]
+print "PYTHONPATH starts with", os.getenv("PYTHONPATH").split(":")[0]
