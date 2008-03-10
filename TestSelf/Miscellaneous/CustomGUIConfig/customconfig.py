a42eb11ff136a0dbe135b4e320fa7842626fa51e
geoff <>
2008-03-10 08:21:24 +0000

    Test no longer relevant after introduction of GUI configuration. Hasn't been working for some time anyway...

--- TestSelf/Miscellaneous/CustomGUIConfig/customconfig.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-
-from default import Config
-# in theory we can override things in here, in practice we just include it...
-import guiplugins
-
-def getConfig(optionMap):
-    return Config(optionMap)
-
