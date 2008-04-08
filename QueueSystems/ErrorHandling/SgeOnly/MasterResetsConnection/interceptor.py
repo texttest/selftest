bc7b8ea15709c3c02bfbddf75a92d8d75d846ec6
geoff <>
2008-04-08 09:50:36 +0000

    Try repeatedly before giving up

--- QueueSystems/ErrorHandling/SgeOnly/MasterResetsConnection/interceptor.py
+++ QueueSystems/ErrorHandling/SgeOnly/MasterResetsConnection/interceptor.py
@@ -7,7 +7,7 @@ class ResettingSocket(origSocket):
     callCount = 0
     def makefile(self, *args, **kwargs):
         ResettingSocket.callCount += 1
-        if ResettingSocket.callCount == 2:
+        if ResettingSocket.callCount >= 2:
             raise socket.error, "Connection reset by peer"
         else:
             return origSocket.makefile(self, *args, **kwargs)
