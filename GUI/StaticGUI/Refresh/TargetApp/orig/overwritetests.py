355c723d295ad56dae7aa6b76490ed9b193985fb
geoff <>
2008-04-18 04:46:21 +0000

    Touch everything so we know it's changed

--- GUI/StaticGUI/Refresh/TargetApp/orig/overwritetests.py
+++ GUI/StaticGUI/Refresh/TargetApp/orig/overwritetests.py
@@ -8,3 +8,7 @@ origDir = os.path.join(ttHome, "orig")
 newDir = os.path.join(ttHome, "new", "subdir")
 shutil.rmtree(origDir)
 os.rename(newDir, origDir)
+for root, dirs, files in os.walk(origDir):
+    for file in files:
+        fullPath = os.path.join(root, file)
+        os.utime(fullPath, None) # touch everything
