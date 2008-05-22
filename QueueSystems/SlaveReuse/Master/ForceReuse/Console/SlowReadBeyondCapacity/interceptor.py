7c214edabb938a388d7624fd983b1ff783e0b8c0
geoff <>
2008-05-22 06:36:10 +0000

    Making the slow read slower to handle overloading at night

--- QueueSystems/SlaveReuse/Master/ForceReuse/Console/SlowReadBeyondCapacity/interceptor.py
+++ QueueSystems/SlaveReuse/Master/ForceReuse/Console/SlowReadBeyondCapacity/interceptor.py
@@ -5,7 +5,7 @@ orig_listdir = os.listdir
 
 def slow_listdir(dirname, *args, **kwargs):
     if dirname.find("Suite/Test") != -1:
-        time.sleep(1)
+        time.sleep(2)
     return orig_listdir(dirname, *args, **kwargs)
 
 os.listdir = slow_listdir
