d5dd1ec879cb569799a3241b3be90ffef551845c
geoff <>
2008-07-01 13:21:38 +0000

    Returning to faking Xvfb, which might not be installed on the machine which is running the tests

--- TestSelf/UITesting/UnixOnly/TargetApp/display.py
+++ TestSelf/UITesting/UnixOnly/TargetApp/display.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
 import os
 
-# Would like to check against parent process ID but it seems there are race conditions around this.
-machine = os.getenv("DISPLAY").split(":")[0]
-print "Using display on", machine
+print "Using display", os.getenv("DISPLAY")
---
+++ TestSelf/UITesting/UnixOnly/interceptor.py
@@ -0,0 +1,8 @@
+
+import os
+
+def getpid():
+    # my first phone number :)
+    return 102283
+
+os.getpid = getpid
