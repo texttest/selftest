f6a3bef6f1732089101574e8eed6fa04312d7f14
henning <>
2008-07-10 11:09:11 +0000

    Added test suite to test the railfleet cofig module. Added three test which today had full coverage of the current code.

---
+++ carmen/railfleet/TargetApp/run_planeditor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# $Header: /carm/2_CVS/Testing/Automatic/texttest/carmen/railfleet/TargetApp/run_planeditor.py,v 1.1 2008/07/10 13:09:14 henning Exp $
+
+#import common
+
+#modelServer = common.ModelServer()
+#modelServer.start()
+print "Started the model server"
+
+
+#planEditor = common.PlanEditor(modelServer.getPort())
+#planEditor.run()
+print "Started the Plan Editor"
+
+#modelServer.close()
