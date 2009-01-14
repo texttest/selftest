ffe11fc178a26c554527f033f138d52886d0d7a7
geoff <>
2009-01-14 09:19:17 +0000

    Don't assume default system versions of Python and GTK, check them explicitly

--- carmen/GtkEnvironment/TargetApp/hello.py
+++ carmen/GtkEnvironment/TargetApp/hello.py
@@ -2,7 +2,18 @@
 
 import gtk, sys, os
 
-print "Python version =", ".".join(map(str, sys.version_info[:3]))
-print "GTK version =", ".".join(map(str, gtk.gtk_version))
+def compareWithDefault(package, version):
+    rpmText = os.popen("rpm -q " + package).read()
+    defaultVersion = rpmText.split("-")[1]
+    if version == defaultVersion:
+        print "Used system default version of", package
+    else:
+        print "Used non-standard", package, "version", version, "instead of version", defaultVersion
+
+pyversion = ".".join(map(str, sys.version_info[:3]))
+gtkversion = ".".join(map(str, gtk.gtk_version))
+
+compareWithDefault("python", pyversion)
+compareWithDefault("gtk2", gtkversion)
 print "LD_LIBRARY_PATH starts with", os.getenv("LD_LIBRARY_PATH").split(":")[0]
 print "PYTHONPATH starts with", os.getenv("PYTHONPATH").split(":")[0]
