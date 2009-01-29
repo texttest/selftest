ff6596d20dccbd57867301636abf452a64b28313
Geoff Bache <geoff.bache@jeppesen.com>
2009-01-29 14:15:59 +0100

    Adding new filter files and removing unnecessary nonlinux filters

--- TestSelf/ErrorHandling/GUI/GtkTooOld/interceptor.py
+++ TestSelf/ErrorHandling/GUI/GtkTooOld/interceptor.py
@@ -1,2 +1,12 @@
-import gtk
-gtk.pygtk_version = 2, 2, 0
+import __builtin__
+
+pygtk_version = 2, 2, 0
+
+origImport = __builtin__.__import__
+def myImport(name, *args, **kwargs):
+    if name == "gtk":
+        return origImport("interceptor", *args, **kwargs)
+    else:
+        return origImport(name, *args, **kwargs)
+
+__builtin__.__import__ = myImport
