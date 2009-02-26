c10a108081305ae0f0e9df6d750f5e33d8fd320b
Geoff Bache <geoff.bache@jeppesen.com>
2009-02-26 22:54:44 +0100

    Reworked version control tests, 13 of them now run against Bazaar as well as CVS

---
+++ VersionControl/NotInstalled/interceptor.py
@@ -0,0 +1,12 @@
+
+import subprocess
+
+origPopen = subprocess.Popen
+
+class MyPopen(origPopen):
+    def __init__(self, cmdArgs, *args, **kwargs):
+        origPopen.__init__(self, cmdArgs, *args, **kwargs)
+        if cmdArgs[0] in [ "cvs", "bzr" ]:
+            raise OSError, "Pretending " + cmdArgs[0].upper() + " isn't installed!"
+        
+subprocess.Popen = MyPopen
