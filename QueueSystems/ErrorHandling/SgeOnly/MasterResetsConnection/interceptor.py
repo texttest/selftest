0780b4ef6759ffc7d29d135e1a0b7eb75d0eecee
Geoff Bache <geoff.bache@jeppesen.com>
2009-03-03 13:57:34 +0100

    Change to exponentially increasing sleep time between attempts, and write exception to stderr so it gets found by master log

--- QueueSystems/ErrorHandling/SgeOnly/MasterResetsConnection/interceptor.py
+++ QueueSystems/ErrorHandling/SgeOnly/MasterResetsConnection/interceptor.py
@@ -1,15 +1,19 @@
 
-import socket
+import socket, time
 
 origSocket = socket.socket
 
 class ResettingSocket(origSocket):
     callCount = 0
-    def makefile(self, *args, **kwargs):
+    def sendall(self, *args, **kwargs):
         ResettingSocket.callCount += 1
         if ResettingSocket.callCount >= 2:
             raise socket.error, "Connection reset by peer"
         else:
-            return origSocket.makefile(self, *args, **kwargs)
+            return origSocket.sendall(self, *args, **kwargs)
+
+def fakeSleep(*args):
+    pass
 
 socket.socket = ResettingSocket
+time.sleep = fakeSleep
