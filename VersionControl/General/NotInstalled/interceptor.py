2eb8232bad9bb7e6d24dcd36df8e63c2a01840ca
Geoff Bache <geoff.bache@jeppesen.com>
2009-04-03 16:03:40 +0200

    New test results for Mercurial

--- VersionControl/General/NotInstalled/interceptor.py
+++ VersionControl/General/NotInstalled/interceptor.py
@@ -6,7 +6,7 @@ origPopen = subprocess.Popen
 class MyPopen(origPopen):
     def __init__(self, cmdArgs, *args, **kwargs):
         origPopen.__init__(self, cmdArgs, *args, **kwargs)
-        if cmdArgs[0] in [ "cvs", "bzr" ]:
+        if cmdArgs[0] in [ "cvs", "bzr", "hg" ]:
             raise OSError, "Pretending " + cmdArgs[0].upper() + " isn't installed!"
         
 subprocess.Popen = MyPopen
--- VersionControl/interceptor.py
+++ VersionControl/interceptor.py
@@ -1,12 +1,12 @@
 
-import datetime
+import datetime, os
 
 origdate = datetime.datetime
 
 class MyDate(origdate):
     @classmethod
     def utcnow(klass):
-        return origdate(2009, 2, 26, 23, 0)
-
+        year, month, day = map(int, os.getenv("TODAYS_DATE_FOR_VCS").split("-"))
+        return origdate(year, month, day, 23, 0)
 
 datetime.datetime = MyDate
