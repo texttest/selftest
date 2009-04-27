6d34ea0cf83bbc67e660f22506d4583d501c6650
Geoff Bache <geoff.bache@jeppesen.com>
2009-04-27 09:43:23 +0200

    No point in calling the original programs anyway, just leads to indeterminism

--- VersionControl/General/NotInstalled/interceptor.py
+++ VersionControl/General/NotInstalled/interceptor.py
@@ -5,7 +5,6 @@ origPopen = subprocess.Popen
 
 class MyPopen(origPopen):
     def __init__(self, cmdArgs, *args, **kwargs):
-        origPopen.__init__(self, cmdArgs, *args, **kwargs)
         if cmdArgs[0] in [ "cvs", "bzr", "hg" ]:
             raise OSError, "Pretending " + cmdArgs[0].upper() + " isn't installed!"
         
