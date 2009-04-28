b077dec4cd42574090be8e37973d37a1f61fc170
Geoff Bache <geoff.bache@jeppesen.com>
2009-04-28 08:07:40 +0200

    Fixing interceptor for Windows

--- VersionControl/General/NotInstalled/interceptor.py
+++ VersionControl/General/NotInstalled/interceptor.py
@@ -5,6 +5,7 @@ origPopen = subprocess.Popen
 
 class MyPopen(origPopen):
     def __init__(self, cmdArgs, *args, **kwargs):
+        self._child_created = False
         if cmdArgs[0] in [ "cvs", "bzr", "hg" ]:
             raise OSError, "Pretending " + cmdArgs[0].upper() + " isn't installed!"
         
