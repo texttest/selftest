850f0ebe94695aff118bf3eed2c316f291b1d806
Geoff Bache <geoff.bache@jeppesen.com>
2009-05-04 11:25:02 +0200

    Trying to stabilise test result across platforms

--- TestSelf/BatchTests/Console/NoSuchServer/interceptor.py
+++ TestSelf/BatchTests/Console/NoSuchServer/interceptor.py
@@ -3,6 +3,6 @@ import smtplib, socket
 
 class SMTP(smtplib.SMTP):
     def connect(self, server):
-        raise socket.gaierror, (-2, 'Name or service not known')
+        raise socket.gaierror, 'Name or service not known'
 
 smtplib.SMTP = SMTP
