f3449af0afae82a12a161206945e972d2998bcf6
Geoff Bache <geoff.bache@jeppesen.com>
2009-05-26 16:07:25 +0200

    Changed logging setup such that nothing goes via standard output any more

--- TestSelf/BatchTests/EmailCollection/IgnoreOldRuns/interceptor.py
+++ TestSelf/BatchTests/EmailCollection/IgnoreOldRuns/interceptor.py
@@ -5,7 +5,7 @@ class SMTP(smtplib.SMTP):
     def connect(self, server):
         pass # Fake this so we don't rely on a mail server to run the tests
     def sendmail(self, fromAddr, toAddresses, contents):
-        print "\nSending mail from address", fromAddr, "to", toAddresses
+        print "Sending mail from address", fromAddr, "to", toAddresses
         fileName =  os.path.join(os.path.dirname(os.getenv("TEXTTEST_TMP")), "targetReport.texttest")
         writeFile = open(fileName, "w")
         writeFile.write(contents)
