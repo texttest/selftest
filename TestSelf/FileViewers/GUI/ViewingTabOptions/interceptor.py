7c2f0bb8d7947a88f8f3a1ab5078a2d710574399
Geoff Bache <geoff.bache@jeppesen.com>
2009-06-10 21:55:04 +0200

    Re-include the locale setting from the root test suite

--- TestSelf/FileViewers/GUI/ViewingTabOptions/interceptor.py
+++ TestSelf/FileViewers/GUI/ViewingTabOptions/interceptor.py
@@ -3,3 +3,13 @@ import gtk, sys
 sys.version_info = 2, 9, 42, "final", 0
 gtk.pygtk_version = 2, 42, 0
 gtk.gtk_version = 2, 58, 42
+
+# Make sure the tests with Swedish chars work correctly
+# on systems with other default locales than ISO8859-1 ...
+
+import locale
+
+def default_locale():
+    return "en_US", "ISO8859-1"
+
+locale.getdefaultlocale = default_locale
