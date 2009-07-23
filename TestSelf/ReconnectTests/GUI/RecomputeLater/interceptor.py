d1b323047b01b9049c6b228ba5920247f7d56501
Geoff Bache <geoff.bache@jeppesen.com>
2009-07-23 09:13:42 +0200

    Trying to make it indepedent of file-edit times

--- TestSelf/ReconnectTests/GUI/RecomputeLater/interceptor.py
+++ TestSelf/ReconnectTests/GUI/RecomputeLater/interceptor.py
@@ -9,15 +9,15 @@ class UptodateStat:
         return getattr(self.origStat, name)
     def __getitem__(self, item):
         origItem = self.origStat[item]
-        if item != stat.ST_MTIME or self.filename.find("TargetApp") != -1:
+        if item != stat.ST_MTIME or not "TargetApp" in self.filename or abs(time.time() - origItem) < 60:
             return origItem
         else:
-            return origItem + 60
+            return origItem - 60 * 60 * 24 * 365
 
 origStat = os.stat
 def myStat(filename):
     info = origStat(filename)
-    if filename.find(".hello") != -1:
+    if "hello" in filename:
         return UptodateStat(info, filename)
     else:
         return info
