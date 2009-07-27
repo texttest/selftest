73722d9c2d403a9cf30681db31d4effc0688b5d9
Geoff Bache <geoff.bache@jeppesen.com>
2009-07-27 11:57:33 +0200

    Standardise exception to prevent failure on other platforms

--- QueueSystems/ErrorHandling/SgeOnly/BadServerAddress/interceptor.py
+++ QueueSystems/ErrorHandling/SgeOnly/BadServerAddress/interceptor.py
@@ -1,7 +1,14 @@
 
-import time
+import socket, time
 
+origSocket = socket.socket
+
+class BadSocket(origSocket):
+    def connect(self, *args, **kwargs):
+        raise socket.error, "Can't connect to non-existent machines!"
+        
 def fakeSleep(*args):
     pass
 
+socket.socket = BadSocket
 time.sleep = fakeSleep
