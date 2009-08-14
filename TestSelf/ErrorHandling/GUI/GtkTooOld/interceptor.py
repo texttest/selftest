274de3c6c18c8eb3732a511061648609cc4a7af2
Geoff Bache <geoff.bache@jeppesen.com>
2009-08-14 08:26:41 +0200

    Making interceptor more resilient

--- TestSelf/ErrorHandling/GUI/GtkTooOld/interceptor.py
+++ TestSelf/ErrorHandling/GUI/GtkTooOld/interceptor.py
@@ -3,9 +3,14 @@ import __builtin__
 class MyGtkModule:
     def __init__(self):
         self.pygtk_version = 2, 2, 0
-        
     def __getattr__(self, name):
         return self
+    def __call__(self, *args):
+        pass
+    def __eq__(self, other):
+        return True
+    def __hash__(self):
+        return 0
 
 origImport = __builtin__.__import__
 def myImport(name, *args, **kwargs):
