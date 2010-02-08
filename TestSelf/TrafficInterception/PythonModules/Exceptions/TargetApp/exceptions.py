276a599c31c162e5dd39d7d5c3417300a2de8c90
Geoff Bache <geoff.bache@jeppesen.com>
2010-02-08 13:38:51 +0100

    Also don't use time module, it's builtin on Windows

--- TestSelf/TrafficInterception/PythonModules/Exceptions/TargetApp/exceptions.py
+++ TestSelf/TrafficInterception/PythonModules/Exceptions/TargetApp/exceptions.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import time
+import logging
 try:
-    print time.no_such_method("argument", faking=True)
+    print logging.no_such_method("argument", faking=True)
 except Exception, e:
     print "Caught exception:", e
 
