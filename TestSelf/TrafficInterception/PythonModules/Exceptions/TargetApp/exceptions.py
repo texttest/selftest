cb4a6e3ff2e5a6708cf33879bbd792a8df2de954
Geoff Bache <geoff.bache@jeppesen.com>
2010-02-08 12:17:23 +0100

    Test for exceptions defined in the intercepted module also

--- TestSelf/TrafficInterception/PythonModules/Exceptions/TargetApp/exceptions.py
+++ TestSelf/TrafficInterception/PythonModules/Exceptions/TargetApp/exceptions.py
@@ -5,3 +5,15 @@ try:
     print time.no_such_method("argument", faking=True)
 except Exception, e:
     print "Caught exception:", e
+
+import urllib2
+try:
+    print urllib2.urlopen("http://no.such.site")
+except urllib2.URLError, e:
+    print "Caught exception:", e
+
+try:
+    print urllib2.urlopen("http://another.no.such.site")
+except Exception, e:
+    print "Caught exception:", e
+
