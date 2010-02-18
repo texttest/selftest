f7757ef67b1f96787b281220c9a29be574298039
Geoff Bache <geoff.bache@jeppesen.com>
2010-02-18 15:39:34 +0100

    Support attribute assignment in python module interception

---
+++ TestSelf/TrafficInterception/PythonModules/Objects/AttributeAssignment/TargetApp/importdir_with_pyc/mymodule.py
@@ -0,0 +1,6 @@
+
+class MyObject:
+    def __init__(self):
+        self.value = "Initial Value"
+    def getValue(self):
+        return self.value
---
+++ TestSelf/TrafficInterception/PythonModules/Objects/AttributeAssignment/TargetApp/myscript.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import mymodule
+
+theObject = mymodule.MyObject()
+theObject.value = "My Value"
+print theObject.getValue()
