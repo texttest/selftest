2cde85d0b5bbbded34c010ebcbc22c4f3cf27993
Geoff Bache <geoff.bache@jeppesen.com>
2010-03-05 14:27:47 +0100

    Add quotes to multiline text

--- TestSelf/TrafficInterception/PythonModules/Strings/ComplexArguments/TargetApp/printtemp.py
+++ TestSelf/TrafficInterception/PythonModules/Strings/ComplexArguments/TargetApp/printtemp.py
@@ -5,7 +5,10 @@ import tempfile
 strVal = u"""This
 is a large unicode string"""
 
+withQuote = """This one
+has a ' in it"""
+
 try:
-    print tempfile.no_such_method({ "string" : strVal })
+    print tempfile.no_such_method({ "string" : strVal, "quotestring": withQuote })
 except Exception, e:
     pass
