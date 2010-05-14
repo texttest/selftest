2fdd64563644cd5a5d7d49dcca2a93fa42ed7362
Geoff Bache <geoff.bache@jeppesen.com>
2010-05-14 17:23:15 +0200

    Poll SGE periodically to pick up dead jobs

--- QueueSystems/ErrorHandling/Console/JobDisappears/interceptor.py
+++ QueueSystems/ErrorHandling/Console/JobDisappears/interceptor.py
@@ -1,7 +1,11 @@
 
 import time
 
+origSleep = time.sleep
+
+# Designed to let the polling thread sleep, but not waiting for the job
 def fakeSleep(seconds):
-    pass
+    if seconds < 1:
+        origSleep(seconds)
 
 time.sleep = fakeSleep
