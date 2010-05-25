91a4d6676af0ff9ecb19b4e246bc4ee1efd1c273
Geoff Bache <geoff.bache@jeppesen.com>
2010-05-25 12:10:38 +0200

    Don't let Unknown Instances take priority over globals

---
+++ TestSelf/TrafficInterception/PythonModules/Strings/ArgumentTransfer/TargetApp/importdir_with_pyc/mymodule.py
@@ -0,0 +1,6 @@
+
+def callFunction(a, b, c, first, second, third):
+    if not c and not first:
+        return a + str(b + second + third)
+    else:
+        return str(c)
---
+++ TestSelf/TrafficInterception/PythonModules/Strings/ArgumentTransfer/TargetApp/myscript.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import mymodule
+
+print mymodule.callFunction("hello", 1, None, third=0.3, second=2, first=None) 
