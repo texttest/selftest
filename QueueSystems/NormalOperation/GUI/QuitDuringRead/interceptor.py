35cd10abf49742b61fe6c5b21e2e3a23344183af
Geoff Bache <geoff.bache@jeppesen.com>
2010-06-29 16:20:15 +0200

    Make test more deterministic: wait a bit longer reading the basic test

--- QueueSystems/NormalOperation/GUI/QuitDuringRead/interceptor.py
+++ QueueSystems/NormalOperation/GUI/QuitDuringRead/interceptor.py
@@ -6,7 +6,7 @@ orig_listdir = os.listdir
 def slow_listdir(dirname, *args, **kwargs):
     basename = os.path.basename(dirname)
     if basename == "Basic2":
-        time.sleep(3)
+        time.sleep(4)
     return orig_listdir(dirname, *args, **kwargs)
 
 os.listdir = slow_listdir
