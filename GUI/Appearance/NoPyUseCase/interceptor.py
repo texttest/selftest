f22e8ac719a11c2f3c6916488424be7ba0f9f6f8
Geoff Bache <geoff.bache@jeppesen.com>
2010-08-06 13:32:53 +0200

    Make things work better if PyUseCase cannot be found

---
+++ GUI/Appearance/NoPyUseCase/interceptor.py
@@ -0,0 +1,11 @@
+
+import __builtin__, sys
+
+origImport = __builtin__.__import__
+def myImport(name, *args, **kwargs):
+    if name == "gtkusecase":
+        raise ImportError, "No module named gtkusecase"
+    else:
+        return origImport(name, *args, **kwargs)
+
+__builtin__.__import__ = myImport
