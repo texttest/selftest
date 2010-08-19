4f01a55c54523788807296d42b84382d4d93df08
Geoff Bache <geoff.bache@jeppesen.com>
2010-08-19 08:10:44 +0200

    Import when needed, reduce race condition risk

--- TestSelf/RunningTests/TargetApp/sleep.py
+++ TestSelf/RunningTests/TargetApp/sleep.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-import sys, time, os, signal
-
+import os, signal
 
 # Don't print stack traces when killed, as Windows can't do this...
 if os.name == "posix":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+import sys
 print "Sleeping for 1000 seconds..."
 sys.stdout.flush()
+import time
 time.sleep(int(os.getenv("SLEEP_TIME", 1000))) # Until we get killed, basically
 print "Done"
