dfa6385d82974fd707e15ae7642f8678ad726a37
Geoff Bache <geoff.bache@jeppesen.com>
2010-08-19 11:26:20 +0200

    Restructured traffic interception tests and added support for unsetting environment

--- TestSelf/TrafficInterception/CommandLine/TargetApp/source/syscalls.py
+++ TestSelf/TrafficInterception/CommandLine/TargetApp/source/syscalls.py
@@ -16,6 +16,7 @@ os.system('firstcall.py -a -b -c "hello | goodbye"')
 # Check our environment and current working directory goes into the traffic environment
 os.environ["MY_ENV_VAR"] = "value"
 os.environ["PATH"] = "/some/extra/path" + os.pathsep + os.getenv("PATH")
+del os.environ["MY_ENV_FOR_UNSET"]
 os.mkdir("tmpdir")
 os.chdir("tmpdir")
 os.system("secondcall.py -c -x")
