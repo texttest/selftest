1544729b9dbfa701f39f0be6631faec8a17f8f33
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-13 13:36:19 +0200

    Transforming encoding tests

--- GUI/DynamicGUI/TextView/Encoding/LocaleMatches/interceptor.py
+++
@@ -1,7 +0,0 @@
-
-import locale
-
-def default_locale():
-    return "en_US", "ISO8859-1"
-
-locale.getdefaultlocale = default_locale
--- GUI/DynamicGUI/TextView/Encoding/LocaleMismatch/interceptor.py
+++
@@ -1,7 +0,0 @@
-
-import locale
-
-def default_locale():
-    return "en_US", "utf"
-
-locale.getdefaultlocale = default_locale
