a39bef7cbf3a8284ab756da0b6488e92de5f5da8
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-22 10:38:48 +0200

    Don't mangle Windows paths in Python interception

--- TestSelf/TrafficInterception/PythonModules/Strings/ComplexArguments/TargetApp/printtemp.py
+++ TestSelf/TrafficInterception/PythonModules/Strings/ComplexArguments/TargetApp/printtemp.py
@@ -8,7 +8,11 @@ is a large unicode string"""
 withQuote = """This one
 has a ' in it"""
 
+windowsPath = "C:\\no_such_directory"
+
 try:
-    print tempfile.gettempprefix({ "string" : strVal, "quotestring": withQuote })
+    print tempfile.gettempprefix({ "string" : strVal,
+                                   "quotestring": withQuote,
+                                   "windows_path": windowsPath })
 except Exception, e:
     pass
