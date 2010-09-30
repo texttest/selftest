f68cc9a6bda48520b4c6f8b339f2b000945823c0
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-30 09:08:54 +0200

    Changes to prevent slow importing causing failures

--- QueueSystems/TargetApp/sleep.py
+++ QueueSystems/TargetApp/sleep.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-import sys, time
-
-def getSleepLength():
-    if len(sys.argv) > 1:
-        return int(sys.argv[1])
-    else:
-        return 5
-
 try:
+    import sys, time
+
+    def getSleepLength():
+        if len(sys.argv) > 1:
+            return int(sys.argv[1])
+        else:
+            return 5
+
     sleepLength = getSleepLength()
     print "Sleeping for", sleepLength, "seconds..."
     sys.stdout.flush()
--- TestSelf/RunningTests/TargetApp/sleep.py
+++ TestSelf/RunningTests/TargetApp/sleep.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-import os, signal
+try:
+    import os, sys, time
 
-# Don't print stack traces when killed, as Windows can't do this...
-if os.name == "posix":
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-import sys
-print "Sleeping for 1000 seconds..."
-sys.stdout.flush()
-import time
-time.sleep(int(os.getenv("SLEEP_TIME", 1000))) # Until we get killed, basically
-print "Done"
+    print "Sleeping for 1000 seconds..."
+    sys.stdout.flush()
+    time.sleep(int(os.getenv("SLEEP_TIME", 1000))) # Until we get killed, basically
+    print "Done"
+except KeyboardInterrupt:
+    # Don't print stack traces when killed, as Windows can't do this...
+    pass
