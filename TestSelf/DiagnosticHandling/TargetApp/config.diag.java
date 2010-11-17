587aed34f5d7d2c6829e644070d8c696a9ed38ed
Geoff Bache <geoff.bache@jeppesen.com>
2010-11-17 21:10:09 +0100

    Removed Java tests: they don't actually test anything useful as they succeed even if the Java-specific stuff is disabled. Need recreating without needing to intercept Java

--- TestSelf/DiagnosticHandling/TargetApp/config.diag.java
+++
@@ -1,9 +0,0 @@
-# Path to the executable
-executable:DiagnosticWriter
-interpreter:java
-
-test_data_properties:myprops
-
-[run_dependent_text]
-output:Java version
-[end]
--- TestSelf/UITesting/UseCaseRecording/TargetApp/config.gui.java
+++
@@ -1,9 +0,0 @@
-# Path to the executable.
-executable:${TEXTTEST_CHECKOUT}/videostore.jar
-
-interpreter:java -jar
-
-use_case_recorder:jusecase
-
-[run_dependent_text]
-errors:colormap entry
