c055edb94e0106729af8a4077eb0f1beedd6f97d
Geoff Bache <geoff.bache@jeppesen.com>
2010-12-07 21:45:05 +0100

    Removing all references to old collect_traffic* settings

--- TestSelf/TrafficInterception/repeat_capturemock_gen.py
+++
@@ -1,7 +0,0 @@
-
-import os, subprocess
-
-for root, dirs, files in os.walk(os.getcwd()):
-    if "TargetApp" in dirs:
-        targetDir = os.path.join(root, "TargetApp")
-        subprocess.call([ "texttest.py", "-d", targetDir, "-s", "traffic.ConvertToCaptureMock" ])
