7dca7dc560ae46bd6c2f6e745cc2b9b2437d5250
Geoff Bache <geoff.bache@jeppesen.com>
2011-02-18 10:16:55 +0100

    Updating test to make use of proxy_options

--- QueueSystems/NormalOperation/GUI/UseProxySubmission/TargetApp/proxy.py
+++ QueueSystems/NormalOperation/GUI/UseProxySubmission/TargetApp/proxy.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import os, subprocess, socket
+import os, subprocess, socket, sys
 
 os.environ["PROXY_HOSTNAME"] = socket.gethostname()
+os.environ["PROXY_ARG"] = sys.argv[1]
 qsubCommandArgs = eval(os.getenv("TEXTTEST_SUBMIT_COMMAND_ARGS"))
 subprocess.call(qsubCommandArgs)
--- QueueSystems/NormalOperation/GUI/UseProxySubmission/TargetApp/writehosts.py
+++ QueueSystems/NormalOperation/GUI/UseProxySubmission/TargetApp/writehosts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, socket
+import os, socket, sys
 
-print "Test on", socket.gethostname()
-print "Proxy on", os.getenv("PROXY_HOSTNAME")
+print "Test on", socket.gethostname(), "with arg", sys.argv[1]
+print "Proxy on", os.getenv("PROXY_HOSTNAME"), "with arg", os.getenv("PROXY_ARG")
