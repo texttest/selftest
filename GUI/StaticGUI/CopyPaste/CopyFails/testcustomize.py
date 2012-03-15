7eef80b1d5dc2af67c4ed204b7e932dca7f65c4e
Geoff Bache <geoff.bache@jeppesen.com>
2012-03-15 14:02:26 +0100

    Fixing up test after tweak to how tests are copied

--- GUI/StaticGUI/CopyPaste/CopyFails/testcustomize.py
+++ GUI/StaticGUI/CopyPaste/CopyFails/testcustomize.py
@@ -3,8 +3,8 @@ import shutil
 
 realcopytree = shutil.copytree
 
-def copytree(src, dst):
-    realcopytree(src, dst)
+def copytree(src, dst, *args, **kw):
+    realcopytree(src, dst, *args, **kw)
     raise OSError, "Permission denied copying " + src
 
 shutil.copytree = copytree
