597b2273119185cf16e15e52037d8fbb07458f3d
Geoff Bache <geoff.bache@jeppesen.com>
2012-10-31 15:27:59 +0100

    Check appropriate environment is sent to copy_test_path_script

--- TestSelf/TestData/Console/UseCopyScript/personaldir/copy_strangely.py
+++ TestSelf/TestData/Console/UseCopyScript/personaldir/copy_strangely.py
@@ -6,3 +6,6 @@ shutil.copytree(sys.argv[1], sys.argv[2])
 for root, dirs, files in os.walk(sys.argv[2]):
     if "readfile" in files:
         os.remove(os.path.join(root, "readfile"))
+
+writeFile = os.path.join(os.getenv("TEXTTEST_LOG_DIR"), "newfile.tdat")
+open(writeFile, "w").close()
