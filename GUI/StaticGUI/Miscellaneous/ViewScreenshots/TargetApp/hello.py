6f7b5b2f63f80ffd2472bc716e8a7c6a935cb6fa
Geoff Bache <geoff.bache@jeppesen.com>
2013-04-15 10:58:19 +0200

    TTT-3098: allow viewing StoryText screenshots directly in the TextTest GUI

---
+++ GUI/StaticGUI/Miscellaneous/ViewScreenshots/TargetApp/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+
+print 'Hello World!'
+
+os.mkdir("screenshots")
+open("screenshots/screenshot1.png", "w").close()
+open("screenshots/screenshot3.png", "w").close()
+# Check sorting
+open("screenshots/screenshot10.png", "w").close()
---
+++ QueueSystems/TargetApp/screenshots.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+
+print 'Sleeping for 1 seconds...'
+print 'Done'
+
+os.mkdir("screenshots")
+open("screenshots/screenshot1.png", "w").close()
+open("screenshots/screenshot3.png", "w").close()
+# Check sorting
+open("screenshots/screenshot10.png", "w").close()
