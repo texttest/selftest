3293278ce2cdb75aa4bbd971bbd594d86a6dc357
Geoff Bache <geoff.bache@jeppesen.com>
2013-09-27 15:39:52 +0200

    TTT-3259: new test to make sure customising the web page doesn't screw up the ordering

---
+++ TestSelf/BatchTests/EmailCollection/CustomWebPage/customconfig.py
@@ -0,0 +1,13 @@
+
+import default
+
+def getConfig(*args):
+    return MyConfig(*args)
+
+class MyConfig(default.Config):
+    def getWebPageResponder(self):
+        return MyWebPageResponder
+
+class MyWebPageResponder(default.batch.WebPageResponder):
+    pass
+    
