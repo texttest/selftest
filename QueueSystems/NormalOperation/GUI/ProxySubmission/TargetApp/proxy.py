38666837263a80c0eeae6f781bf8f78cd788828f
Geoff Bache <geoff.bache@jeppesen.com>
2014-03-06 17:37:28 +0100

    TTT-3406 test adjustments

--- QueueSystems/NormalOperation/GUI/ProxySubmission/TargetApp/proxy.py
+++ QueueSystems/NormalOperation/GUI/ProxySubmission/TargetApp/proxy.py
@@ -2,7 +2,19 @@
 
 import os, subprocess, socket, sys
 
+def getSlaveLocation(cmdLine):
+    args = cmdLine.split()
+    for i, arg in enumerate(args):
+        if arg == "-slave":
+            return args[i + 1] 
+
 os.environ["PROXY_HOSTNAME"] = socket.gethostname()
 os.environ["PROXY_ARG"] = sys.argv[1]
 qsubCommandArgs = eval(os.getenv("TEXTTEST_SUBMIT_COMMAND_ARGS"))
-subprocess.call(qsubCommandArgs)
+ttCommandArg = qsubCommandArgs[-1]
+slaveLoc = getSlaveLocation(ttCommandArg)
+rootDir = os.path.join(slaveLoc, "slavelogs")
+if not os.path.isdir(rootDir):
+    os.makedirs(rootDir)
+errFile = os.path.join(rootDir, os.getenv("JOB_NAME") + ".errors")
+subprocess.call(qsubCommandArgs, stderr=open(errFile, "w"))
