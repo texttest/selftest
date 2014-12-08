6631051f24c077c0b715d7ba8936c8763bc5ed64
Geoff Bache <geoff.bache@jeppesen.com>
2014-12-08 15:15:49 +0100

    TTT-3503: if test is killed during a rerun, show the original result

---
+++ TestSelf/KnownBugs/GUI/KillDuringRerun/TargetApp/hello.py
@@ -0,0 +1,12 @@
+
+import os, time
+
+filePath = os.path.join(os.getenv("TEXTTEST_SANDBOX_ROOT"), "file")
+
+if os.path.isfile(filePath):
+    time.sleep(1000)
+    print "There is a file!"
+else:
+    open(filePath, "w").close()
+    print "There was no file"
+
