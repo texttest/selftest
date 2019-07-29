4d6f696483359083729851c93b3d0946fde04614
Geoff bache <geoff.bache@pobox.com>
2019-07-29 17:59:33 +0200

    More migration to python 3

--- TestSelf/Miscellaneous/MultipleInterpreters/TargetApp/argreverser.py
+++ TestSelf/Miscellaneous/MultipleInterpreters/TargetApp/argreverser.py
@@ -1,6 +1,7 @@
 print("First line should not be printed!")
 import sys, subprocess
 print("ArgReverser has args", sys.argv)
+sys.stdout.flush()
 subargs = sys.argv[4:]
 subargs.reverse()
-subprocess.call([ "python", sys.argv[3] ] + subargs)
+subprocess.call([ sys.executable, sys.argv[3] ] + subargs)
