7bfd7a9e844d8a15297708c002966b6443c9648e
Geoff bache <geoff.bache@pobox.com>
2019-07-31 15:53:01 +0200

    Migreated Diagnostics test to logging module

--- TestSelf/DiagnosticHandling/TargetApp/source_with_pyc/writediags.py
+++ TestSelf/DiagnosticHandling/TargetApp/source_with_pyc/writediags.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-import os, log4py, time
+import os, logging.config, time
 
 time.sleep(int(os.getenv("SLEEP_TIME", "0")))
 print("This is a simple diagnostic-writing program")
 
-# Point log4py at the configuration file to read
-rootLogger = log4py.create_logger(log4py.TRUE, os.getenv("TESTDIAG_READFILE"))
+logConfigFile = os.getenv("TESTDIAG_READFILE")
+defaults = { "LOCAL_DIR" : os.path.dirname(os.path.abspath(logConfigFile)) }            
+rootLogger = logging.config.fileConfig(logConfigFile, defaults)
 
-logger1 = log4py.Logger().get_instance("firstdiag")
+logger1 = logging.getLogger("firstdiag")
 logger1.info("Some information")
 logger1.debug("Some debug stuff")
 
-logger2 = log4py.Logger().get_instance("seconddiag")
+logger2 = logging.getLogger("seconddiag")
 logger2.info("Some information")
 logger2.debug("Some debug stuff")
 
-logger3 = log4py.Logger().get_instance("thirddiag")
+logger3 = logging.getLogger("thirddiag")
 logger3.info("Some information")
 logger3.debug("Some debug stuff")
