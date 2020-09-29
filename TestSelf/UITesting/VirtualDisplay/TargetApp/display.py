fe8e7ce29cdbd9951e313fccbe8fb1caa947dc4c
Johan Gill <johan.gill@jeppesen.com>
2020-09-29 15:30:35 +0200

    Fix python3 print function

--- TestSelf/UITesting/VirtualDisplay/TargetApp/display.py
+++ TestSelf/UITesting/VirtualDisplay/TargetApp/display.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-import os, time
+
+import os
+import time
+
 
 for var, value in os.environ.items():
     if var.startswith("DISPLAY"):
-        print "Using", var.lower(), value
+        print("Using", var.lower(), value)
 time.sleep(int(os.getenv("TO_SLEEP", "0")))
