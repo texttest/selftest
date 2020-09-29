da01b25852cfe192cbacca151bc0fe7a63925f2d
Johan Gill <johan.gill@jeppesen.com>
2020-09-29 15:32:28 +0200

    Fix issue where listdir is called with a file descriptor
    Shutil on python3 calles listdir with a file descriptor (int)

--- QueueSystems/SlaveReuse/Master/ForceReuse/Console/SlowReadBeyondCapacity/testcustomize.py
+++ QueueSystems/SlaveReuse/Master/ForceReuse/Console/SlowReadBeyondCapacity/testcustomize.py
@@ -4,7 +4,7 @@ import os, time, sys
 orig_listdir = os.listdir
 
 def slow_listdir(dirname, *args, **kwargs):
-    if dirname.find("Suite/Test") != -1:
+    if isinstance(dirname, int) or dirname.find("Suite/Test") != -1:
         time.sleep(2)
     return orig_listdir(dirname, *args, **kwargs)
 
