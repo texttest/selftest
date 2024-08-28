bec48825ef8a5d2477dfc53d4f95181582e57f02
Geoff Bache <geoff.bache@t-hive.io>
2024-08-28 15:06:18 +0200

    Sort for deterministic behaviour across Python versions

--- TestSelf/Mocking/CaptureMock/Console/EnvironmentOnly/TargetApp/cpmock_env.py
+++ TestSelf/Mocking/CaptureMock/Console/EnvironmentOnly/TargetApp/cpmock_env.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 import os
-for key, value in os.environ.items():
+for key, value in sorted(os.environ.items()):
     if key.startswith("TEXTTEST_CAPTUREMOCK_"):
         print(key, "=", value)
