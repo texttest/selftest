c83db367a53dc72feae59c03bc41e70ef67fa52a
Geoff Bache <geoff.bache@qualisys.se>
2025-02-21 11:14:23 +0100

    Fixing rundep and fpdiff tests to assume virtual environment

--- testrigs/filter_fpdiff.py
+++ testrigs/filter_fpdiff.py
@@ -5,21 +5,9 @@ import os
 import sys
 import io
 import difflib
-
-
-def fixSysPath(fileName):
-    install_root = os.path.dirname(os.path.dirname(fileName))
-    # We pick up the basic libraries.
-    # or a "generic" directory containing the TextTest core with local modules in the root
-    for subdir in ["", "lib", "site/lib", "generic"]:
-        libDir = os.path.abspath(os.path.join(install_root, subdir))
-        if os.path.isdir(libDir):
-            sys.path.insert(0, libDir)
-
+from texttestlib.default.fpdiff import fpfilter
 
 def main():
-    fixSysPath(os.path.abspath(__file__))
-    from texttestlib.default.fpdiff import fpfilter
     parser = optparse.OptionParser("usage: %prog [options] fromfile tofile")
     parser.add_option("-t", "--tolerance", type="float",
                       help='Set absolute floating point tolerance')
--- testrigs/filter_rundependent.py
+++ testrigs/filter_rundependent.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python3
 
-
-import os
 import sys
 from optparse import OptionParser
-
-
-def fixSysPath(fileName):
-    install_root = os.path.dirname(os.path.dirname(fileName))
-    # We pick up the basic libraries.
-    # or a "generic" directory containing the TextTest core with local modules in the root
-    for subdir in ["", "lib", "site/lib", "generic"]:
-        libDir = os.path.abspath(os.path.join(install_root, subdir))
-        if os.path.isdir(libDir):
-            sys.path.insert(0, libDir)
-
-
+from texttestlib import plugins, default
+    
 if __name__ == "__main__":
-    fixSysPath(os.path.abspath(__file__))
-    from texttestlib import plugins, default
     parser = OptionParser("usage: %prog [options] filter1 filter2 ...")
     parser.add_option("-m", "--module",
                       help="also import module MODULE", metavar="MODULE")
